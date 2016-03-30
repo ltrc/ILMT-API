d296f90a472d9dfebb892b2727be0455b4a0404f
Nehal J Wani <nehaljw.kkd1@gmail.com>
2016-03-31 02:27:31 +0530

    Add Prune to HIN-PAN

--- lib/ILMT/HIN/PAN.pm
+++ lib/ILMT/HIN/PAN.pm
@@ -4,6 +4,6 @@ use warnings;
 use Data::Dumper;
 use ILMT::Translator;
 
-my @seq = ("Tokenizer", "UTF2WX", "Morph", "POSTagger", "Chunker");
+my @seq = ("Tokenizer", "UTF2WX", "Morph", "POSTagger", "Chunker", "Prune");
 
 my $langpair_obj = new_translator ILMT::Translator("HIN", "PAN", \@seq);

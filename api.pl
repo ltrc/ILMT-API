bab5091b1efa419c2f1ed0f1236aa339ad35fe09
Nehal J Wani <nehaljw.kkd1@gmail.com>
2016-04-15 14:45:27 +0530

    Fork 'em all

--- api.pl
+++ api.pl
@@ -8,21 +8,32 @@ use lib __DIR__ . "/lib";
 use ILMT::Translator qw(get_translator);
 use ILMT::HIN::PAN;
 
+plugin qw(Mojolicious::Plugin::ForkCall);
+
 any '/:src/:tgt/translate' => sub {
-    my $c = shift;
+    my $c = shift->render_later;
+    $c->inactivity_timeout(3600);
     my %args = %{$c->req->params->to_hash};
     $args{'src_lang'} = $c->param('src');
     $args{'tgt_lang'} = $c->param('tgt');
-    my $translator = get_translator(uc($c->param('src')), uc($c->param('tgt')));
-    my $final_result = $translator->translate(%args);
-    if (exists $args{"pretty"}) {
-        my $final_string = join "\n", map { "$_:\n$final_result->{$_}" } keys %$final_result;
-        $c->render(template => 'pretty', result => $final_string);
-    } else {
-        $c->render(json => $final_result);
-    }
+    $c->fork_call(
+        sub {
+            my (%args) = @_;
+            my $translator = get_translator(uc($c->param('src')), uc($c->param('tgt')));
+            return $translator->translate(%args);
+        },
+        [%args],
+        sub {
+            my ($c, $final_result) = @_;
+            if (exists $args{"pretty"}) {
+                my $final_string = join "\n", map { "$_:\n$final_result->{$_}" } keys %$final_result;
+                $c->render(template => 'pretty', result => $final_string);
+            } else {
+                $c->render(json => $final_result);
+            }
+        }
+    );
 };
-
 
 app->start;
 __DATA__

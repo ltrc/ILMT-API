3ceaa0cf3e023fe4c5529575f18280669c3f107e
Nehal J Wani <nehaljw.kkd1@gmail.com>
2016-04-21 00:39:19 +0530

    Add ILMT::HIN::URD::UTF2WX

---
+++ lib/ILMT/HIN/URD.pm
@@ -0,0 +1,12 @@
+package ILMT::HIN::URD;
+use strict;
+use warnings;
+use Data::Dumper;
+use ILMT::Translator;
+
+my @seq = (
+    "Tokenizer",
+    "UTF2WX",
+);
+
+my $langpair_obj = new_translator ILMT::Translator("HIN", "URD", \@seq);
